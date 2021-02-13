c1c7a0d3b07266fb8c9728eb1468585864a9a97b
Mariazo <zmbereza@gmail.com>
2021-02-13 09:28:54 +0300

    Первые правки

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
-(function() {
+(function () {
   var accordionElements = document.querySelectorAll('.accordion');
 
-  Array.prototype.forEach.call(accordionElements, function(accordion) {
+  Array.prototype.forEach.call(accordionElements, function (accordion) {
     var accordionButton = accordion.querySelector('.accordion__button');
 
     accordion.classList.add('accordion_closed');
-    accordionButton.addEventListener('click', function() {
+    accordionButton.addEventListener('click', function () {
       onAccordionButtonClick(accordion);
     });
   });
 
-  var onAccordionButtonClick = function(accordion) {
+  var onAccordionButtonClick = function (accordion) {
     accordion.classList.toggle('accordion_closed');
     accordion.classList.toggle('accordion_opened');
   };
